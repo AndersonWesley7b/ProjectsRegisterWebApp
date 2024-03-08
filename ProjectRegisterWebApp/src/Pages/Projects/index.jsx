ca3e7b02ceca7c3fa10e74d93b017af95432d2ed
Anderson Terra <AWTFILHO@SMARAPD.COM.BR>
2024-03-08 11:12:42 -0300

    Skeleton adicionado

--- ProjectRegisterWebApp/src/Pages/Projects/index.jsx
+++ ProjectRegisterWebApp/src/Pages/Projects/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { GetAllProjects } from "./Services/ProjectServices"
-import { Box, Card, Grid, Paper, styled } from "@mui/material";
+import { Box, Card, Grid, Paper, styled, Skeleton } from "@mui/material";
 import ProjectCard from "./Components/ProjectCard";
 
 function Projects() {
@@ -31,26 +31,47 @@ function Projects() {
       <Card sx={{
         mt: 2
       }}>
-        <Grid container spacing={4}>
-          <Grid item xs={3}>
-            <Box
-              sx={{
-                p: 4,
-                borderRadius: 2,
-                margin: 2,
-                bgcolor: 'background.default',
-                display: 'grid',
-                gridTemplateColumns: { md: '1fr 1fr' },
-                gap: 2,
-              }}
-            >
-              {Projects.map((project) => (
+        <Grid container spacing={1}>
+          {Projects.length > 0 ? Projects.map((project) => (
+            <Grid item xs={6} sm={4} xl={3}>
+              <Box
+                sx={{
+                  height: 300,
+                  p: 1,
+                  borderRadius: 2,
+                  margin: 1,
+                  bgcolor: 'background.default',
+                  display: 'grid',
+                  gridTemplateColumns: { md: '1fr 1fr' },
+                  gap: 2,
+                }}
+              >
+
                 <Item key={project.ProjectId} elevation={5}>
                   <ProjectCard Project={project} />
                 </Item>
-              ))}
-            </Box>
-          </Grid>
+              </Box>
+            </Grid>
+          )) : (new Array(12).fill().map((_, index) => (
+            <Grid item xs={6} sm={4} xl={3} key={index}>
+              <Box
+                sx={{
+                  height: 300,
+                  p: 1,
+                  borderRadius: 2,
+                  margin: 1,
+                  bgcolor: 'background.default',
+                  display: 'grid',
+                  gridTemplateColumns: { md: '1fr 1fr' },
+                  gap: 2,
+                }}
+              >
+                <Skeleton sx={{
+                  borderRadius: 2,
+                }} width="208.5%" height={283} variant="rectangular" />
+              </Box>
+            </Grid>
+          )))}
         </Grid>
       </Card>
     </>
